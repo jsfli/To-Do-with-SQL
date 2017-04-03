70d338696839ebdcfb9d7d6009595390d01d9c3d
Jeffrey Li <jeffrey.li@cantab.net>
2017-04-03 13:18:13 +0800

    got integration spec to work until Find and One-to-Many section

--- app.rb
+++ app.rb
@@ -1,7 +1,28 @@
-require("sinatra")
-require ("sinatra/reloader")
-also_reload("lib/**/*.rb")
-require(".lib/task")
-require("pg")
+require('sinatra')
+  require('sinatra/reloader')
+  require('./lib/task')
+  require('./lib/list')
+  also_reload('lib/**/*.rb')
+  require("pg")
 
-DB=PG.connect({:dbname => "to_do"})
+DB=PG.connect({:dbname => "to_do_test"}) #change back to to_do after integration testing
+
+get('/') do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+get('/lists') do
+  @lists = List.all()
+  erb(:lists)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+end
